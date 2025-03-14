c568ba2d3914067e279f0184c74c94856a096b4e
icosohedral <catcher.in.the.rye.v@gmail.com>
2025-03-14 15:52:55 +0800

    add index

--- backend/vercel_app/urls.py
+++ backend/vercel_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from vercel_app import api
+from vercel_app import api, page
 
 # configuration --> boslife config
 # subscription --> subscrib from this site
 urlpatterns = [
+    # index
+    path('', page.index, name='index'),
     # api
     path('api/get_model_list', api.get_model_list, name='get_model_list'),
 ]
